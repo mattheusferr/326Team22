388aa3c06b5718894672171efa2126bf99782522
jayanitripathi <jayanitripat@umass.edu>
2024-04-24 21:13:20 -0400

    updated

--- src/client/RoomReservationscript.js
+++ src/client/RoomReservationscript.js
@@ -12,3 +12,28 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error('Error retrieving user:', err);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const grid = document.getElementById("grid");
+    const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  
+    for (let i = 0; i < 12; i++) {
+      const row = document.createElement("div");
+      row.classList.add("grid-row");
+  
+      // Add grid items for each day of the week
+      daysOfWeek.forEach(day => {
+        const cell = document.createElement("div");
+        cell.classList.add("grid-item");
+        if (i === 0) {
+          cell.textContent = day;
+          cell.classList.add("day-of-week");
+        } else {
+          cell.textContent = (i - 1) * 7 + daysOfWeek.indexOf(day) + 1;
+        }
+        row.appendChild(cell);
+      });
+  
+      grid.appendChild(row);
+    }
+  });
