fbb12bf46c57be77e0cd4107cdb85261e7ccad0b
mattheusferr <91915080+mattheusferr@users.noreply.github.com>
2024-05-14 21:26:56 -0400

    Update loginScript.js

--- src/client/loginScript.js
+++ src/client/loginScript.js
@@ -11,16 +11,13 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         loginError.textContent = 'Both fields are required!';
         loginError.style.display = 'block';
     } else { 
-        saveUser(username)
-            .then(() => {
-                loginError.style.display = 'none';
-                console.log('Logging in with:', username, password);
-                alert('Login successful! Welcome, ' + username);
-                window.location.href = '/home.html';
-            })
-            .catch(err => {
-                loginError.textContent = 'Error saving user: ' + err;
-                loginError.style.display = 'block';
-            });
+        //saves username to local db
+        saveUser(username);
+        loginError.style.display = 'none';
+        console.log('Logging in with:', username, password);
+        // Success login:
+        alert('Login successful! Welcome, ' + username);
+        // Redirect after log in
+        window.location.href = '/home.html'; 
     }
 });
